3db30586ed677d3547fe647391eb4299c23df9ef
ChriSki <skibinski.k@gmail.com>
2020-05-29 10:40:55 +0200

    Create MouseTool.bat

---
+++ work/MouseTool.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Java\jdk1.8.0_191\bin\javaw.exe" -classpath "C:\workspace\src\TestProject\out\production\TestProject" com.company.tool.MouseTool
+REM "C:\Program Files\Java\jdk1.8.0_191\bin\java.exe" -classpath "C:\workspace\src\TestProject\out\production\TestProject" main.java.tool.com.company.MouseTool
+REM "C:\Program Files\Java\jdk1.8.0_191\bin\javaw.exe" -classpath "C:\workspace\src\TestProject\out\production\TestProject" com.company.tool.MouseTool
