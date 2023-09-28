23d68fdcd4a883e63b42e6bf94573a91883cd792
ChriSki <skibinski.k@gmail.com>
2023-09-28 20:15:18 +0200

    Create tool.bat

---
+++ tool.bat
@@ -0,0 +1,7 @@
+C:\workspace\core\jdk-11.0.14+9\bin\javaw -cp C:\workspace\src\krzysztofskibinski_priv\TestProject\target\test-app-0.0.1.jar com.company.tool.MouseTool
+REM C:\workspace\core\jdk-11.0.14+9\bin\javaw -jar C:\workspace\src\krzysztofskibinski_priv\TestProject\target\test-app-0.0.1.jar
+REM START "" java -jar C:\workspace\src\krzysztofskibinski_priv\TestProject\target\test-app-0.0.1.jar
+REM java -jar C:\workspace\src\krzysztofskibinski_priv\TestProject\target\test-app-0.0.1.jar
+REM "C:\workspace\core\jdk-11.0.1\bin\javaw.exe" -classpath "C:\workspace\src\krzysztofskibinski_priv\TestProject\out\production\TestProject" com.company.tool.com.MouseTool
+REM "C:\workspace\core\jdk-11.0.1\bin\java.exe" -classpath "C:\workspace\src\TestProject\out\production\TestProject" main.java.tool.com.company.com.MouseTool
+REM "C:\workspace\core\jdk-11.0.1\bin\javaw.exe" -classpath "C:\workspace\src\TestProject\out\production\TestProject" com.company.tool.com.MouseTool
